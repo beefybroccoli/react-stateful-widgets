8747dabb82d967d773b6e1bde88d1589b3dbc5a2
beefybroccoli <tedkimemail@yahoo.com>
2021-09-05 01:15:10 -0700

    no comment

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,35 +34,54 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from "react"; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [stateInputValue, set_stateInputValue] = useState("");
 
-  const changeInput = evt => {
+  const changeInput = (evt) => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
 
     /* STEP 4 */
+    set_stateInputValue(value);
   };
   const reset = () => {
     /* STEP 5 */
+    set_stateInputValue("");
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: stateInputValue.length > 10 ? "crimson" : "royalblue",
+    /*
+     STEP 2
+     Make the color of the text be crimson if the length of 'inputValue' goes over ten.
+    */
   };
 
   return (
-    <div className='widget-input container'>
+    <div className="widget-input container">
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id="output" style={style}></div>
+      {
+        /* STEP 3 - Interpolate the value of the input inside this <div />. How can we make it show in ALL CAPS? */
+        stateInputValue.toUpperCase()
+      }
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input
+          id="input"
+          type="text"
+          onChange={changeInput}
+          value={stateInputValue}
+        />{" "}
+        {/* STEP 6 */}
+        <button id="resetInput" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -22,10 +22,7 @@ STEP 1:
   The 'spinnerOn' slice should be initialized to true so the spinner is visible on page load.
 
 STEP 2:
-  This is called a logical expression. If the expressions on both sides of the '&&' are truthy,
-  the one on the right becomes the value of the whole line. If an expression on either side of the '&&'
-  is falsy, the one on the left becomes the value of the whole line. It's a neat little trick to render
-  a React element (in this case the spinner) conditionally: only if the variable on the left is truthy.
+  This is called a logical expression. If the expressions on both sides of the '&&' are truthy,  the one on the right becomes the value of the whole line. If an expression on either side of the '&&' is falsy, the one on the left becomes the value of the whole line. It's a neat little trick to render a React element (in this case the spinner) conditionally: only if the variable on the left is truthy.
 
   Replace the hard-coded 'true' with the variable that keeps track of whether spinner is on or not.
 
@@ -37,23 +34,29 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from "react"; /* STEP 0 */
 
 export default function Spinner() {
-/* STEP 1 */
+  /* STEP 1 */
+  const [stateSpinner, set_stateSpinner] = useState(true);
 
   const toggleSpinner = () => {
-  /* STEP 4 */
+    /* STEP 4 */
+    set_stateSpinner(!stateSpinner);
   };
 
   return (
-    <div className='widget-spinner container'>
+    <div className="widget-spinner container">
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        true && (
+          <div id="spinner" className="spinner">
+            {stateSpinner && "--+--"}
+          </div>
+        ) /* STEP 2 */
       }
-      <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+      <button id="toggleSpinner" onClick={toggleSpinner}>
+        {stateSpinner ? "Hide Spinner" : "Show Spinner"} {/* STEP 3 */}
       </button>
     </div>
   );
